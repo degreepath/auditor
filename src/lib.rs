e031e1822646e418eef5651bf0d6b0a72c5e8640
Hawken Rives <hawkrives@fastmail.fm>
2019-01-06 18:31:11 -0600

    initial attempt at parsing code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,266 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde_yaml;
+
+fn serde_false() -> bool {
+    false
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+
+    extern crate serde_yaml;
+
+    use crate::Point;
+
+    #[test]
+    fn serde() {
+        let point = Point { x: 1.0, y: 2.0 };
+
+        let s = serde_yaml::to_string(&point).unwrap();
+        assert_eq!(s, "---\nx: 1.0\ny: 2.0");
+
+        let deserialized_point: Point = serde_yaml::from_str(&s).unwrap();
+        assert_eq!(point, deserialized_point);
+    }
+
+    use crate::CountOfEnum;
+
+    #[test]
+    fn count_of_parse() {
+        let count = CountOfEnum::Any;
+
+        let s = serde_yaml::to_string(&count).unwrap();
+        assert_eq!(s, "---\nany");
+
+        let s = "---\nany";
+        let deserialized_count: CountOfEnum = serde_yaml::from_str(&s).unwrap();
+        assert_eq!(deserialized_count, count);
+
+//        let count_2 = CountOfEnum::Number(6);
+//
+//        let s = serde_yaml::to_string(&count_2).unwrap();
+//        assert_eq!(s, "---\n6");
+    }
+}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct AreaOfStudy {
+    #[serde(rename = "name")]
+    area_name: String,
+    #[serde(rename = "type")]
+    area_type: String,
+    catalog: String,
+
+    result: Rule,
+    requirements: Vec<Section>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+enum Rule {
+    Course(CourseRule),
+    Requirement(RequirementRule),
+    CountOf(CountOfRule),
+    Both(BothRule),
+    Either(EitherRule),
+    Given(GivenRule),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct CourseRule {
+    department: String,
+    number: u16,
+    term: Option<String>,
+    section: Option<String>,
+    year: Option<u16>,
+    semester: Option<String>,
+    lab: Option<bool>,
+    international: Option<bool>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct RequirementRule {
+    requirement: String,
+    #[serde(default = "serde_false")]
+    optional: bool,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+enum CountOfEnum {
+    // TODO: get "all" and "any" to parse into the enum properly
+    #[serde(rename = "all")]
+    All,
+    #[serde(rename = "any")]
+    Any,
+    Number(u8),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct CountOfRule {
+    count: CountOfEnum,
+    of: Vec<Rule>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct BothRule {
+    both: (Box<Rule>, Box<Rule>),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct EitherRule {
+    either: (Box<Rule>, Box<Rule>),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+enum GivenRule {
+    AllCourses(GivenAllCoursesRule),
+    TheseCourses(GivenTheseCoursesRule),
+    TheseRequirements(GivenTheseRequirementsRule),
+    AreasOfStudy(GivenAreasOfStudyRule),
+    NamedVariable(GivenNamedVariableRule),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct GivenAllCoursesRule {
+    given: String,
+    what: GivenWhatToGiveEnum,
+    #[serde(rename = "where")]
+    filter: Vec<GivenWhereClause>,
+    limit: Vec<GivenLimiter>,
+    #[serde(rename = "do")]
+    do_action: RuleAction,
+    do_lhs: String,
+    do_operator: RuleOperator,
+    do_rhs: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct GivenTheseCoursesRule {
+    given: String,
+    courses: Vec<CourseRule>,
+    #[serde(rename = "where")]
+    filter: Vec<GivenWhereClause>,
+    limit: Vec<GivenLimiter>,
+    what: GivenWhatToGiveEnum,
+    #[serde(rename = "do")]
+    do_action: RuleAction,
+    do_lhs: String,
+    do_operator: RuleOperator,
+    do_rhs: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct GivenTheseRequirementsRule {
+    given: String,
+    requirements: Vec<RequirementRule>,
+    #[serde(rename = "where")]
+    filter: Vec<GivenWhereClause>,
+    limit: Vec<GivenLimiter>,
+    what: GivenWhatToGiveEnum,
+    #[serde(rename = "do")]
+    do_action: RuleAction,
+    do_lhs: String,
+    do_operator: RuleOperator,
+    do_rhs: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct GivenNamedVariableRule {
+    given: String,
+    #[serde(rename = "where")]
+    filter: Vec<GivenWhereClause>,
+    limit: Vec<GivenLimiter>,
+    what: GivenWhatToGiveEnum,
+    #[serde(rename = "do")]
+    do_action: RuleAction,
+    do_lhs: String,
+    do_operator: RuleOperator,
+    do_rhs: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct GivenWhereClause {}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct GivenLimiter {}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct GivenAreasOfStudyRule {
+    given: String,
+    #[serde(rename = "where")]
+    filter: Vec<GivenWhereClause>,
+    limit: Vec<GivenLimiter>,
+    what: GivenWhatToGiveAreasEnum,
+    #[serde(rename = "do")]
+    do_action: RuleAction,
+    do_lhs: String,
+    do_operator: RuleOperator,
+    do_rhs: String,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+enum GivenWhatToGiveEnum {
+    Courses,
+    DistinctCourses,
+    Credits,
+    Terms,
+    Grades,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+enum GivenWhatToGiveAreasEnum {
+    #[serde(rename = "areas of study")]
+    AreasOfStudy,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+enum RuleOperator {
+    LessThan,
+    EqualTo,
+    GreaterThan,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+enum RuleAction {
+    Count,
+    Sum,
+    Average,
+    Minimum,
+    Difference,
+    NamedVariable,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Requirement {
+    message: String,
+    #[serde(default = "serde_false")]
+    department_audited: bool,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+enum Section {
+    Subsection(Subsection),
+    Requirement(Requirement),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Subsection {
+
 }
