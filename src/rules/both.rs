34170e405bed090d5328e6ce54f0a93b49a1d85b
Hawken Rives <rives@stolaf.edu>
2019-01-08 10:52:50 -0600

    add basic serde tests for BothRule

--- src/rules/both.rs
+++ src/rules/both.rs
@@ -4,3 +4,61 @@ use crate::rules::Rule;
 pub struct BothRule {
     pub both: (Box<Rule>, Box<Rule>),
 }
+
+mod tests {
+    use super::*;
+    use crate::rules::{requirement, Rule};
+
+    #[test]
+    fn serialize() {
+        let input = BothRule {
+            both: (
+                Box::new(Rule::Requirement(requirement::RequirementRule {
+                    requirement: String::from("Name"),
+                    optional: false,
+                })),
+                Box::new(Rule::Requirement(requirement::RequirementRule {
+                    requirement: String::from("Name 2"),
+                    optional: false,
+                })),
+            ),
+        };
+
+        let expected_str = "---
+both:
+  - requirement: Name
+    optional: false
+  - requirement: Name 2
+    optional: false";
+
+        let actual = serde_yaml::to_string(&input).unwrap();
+        assert_eq!(actual, expected_str);
+    }
+
+    #[test]
+    fn deserialize() {
+        let input = "---
+both:
+  - requirement: Name
+    optional: false
+  - requirement: Name 2
+    optional: false";
+        
+        let expected_struct = BothRule {
+            both: (
+                Box::new(Rule::Requirement(requirement::RequirementRule {
+                    requirement: String::from("Name"),
+                    optional: false,
+                })),
+                Box::new(Rule::Requirement(requirement::RequirementRule {
+                    requirement: String::from("Name 2"),
+                    optional: false,
+                })),
+            ),
+        };
+
+
+        let actual: BothRule = serde_yaml::from_str(&input).unwrap();
+        assert_eq!(actual, expected_struct);
+    }
+}
