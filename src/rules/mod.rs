07067508a889b7f452e11ed56645f2a8338773df
Hawken Rives <hawkrives@fastmail.fm>
2019-01-07 19:18:39 -0600

    add overall tests for Rule

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -23,67 +23,205 @@ mod tests {
 
     #[test]
     fn serialize() {
+        let course_a = course::CourseRule {
+            course: "ASIAN 101".to_string(),
+            section: None,
+            term: None,
+            semester: None,
+            year: None,
+            international: None,
+            lab: None,
+        };
+        let course_b = course::CourseRule {
+            course: "ASIAN 101".to_string(),
+            section: None,
+            term: Some("2014-1".to_string()),
+            semester: None,
+            year: None,
+            international: None,
+            lab: None,
+        };
         let data = vec![
-            Rule::Course(course::CourseRule {
-                course: "ASIAN 101".to_string(),
-                section: None,
-                term: None,
-                semester: None,
-                year: None,
-                international: None,
-                lab: None,
-            }),
+            Rule::Course(course_a.clone()),
+            Rule::Course(course_b.clone()),
             Rule::Requirement(requirement::RequirementRule {
                 requirement: "Name".to_string(),
                 optional: true,
             }),
+            Rule::CountOf(count_of::CountOfRule {
+                count: count_of::CountOfEnum::Number(1),
+                of: vec![Rule::Course(course_a.clone())],
+            }),
+            Rule::Both(both::BothRule {
+                both: (
+                    Box::new(Rule::Course(course_a.clone())),
+                    Box::new(Rule::Course(course_b.clone())),
+                ),
+            }),
+            Rule::Either(either::EitherRule {
+                either: (
+                    Box::new(Rule::Course(course_a.clone())),
+                    Box::new(Rule::Course(course_b.clone())),
+                ),
+            }),
+            Rule::Given(given::GivenRule::AllCourses(given::GivenAllCoursesRule {
+                given: "courses".to_string(),
+                what: given::GivenWhatToGiveEnum::Courses,
+                filter: vec![],
+                limit: vec![],
+                action: given::DoAction {
+                    command: given::RuleAction::Count,
+                    lhs: "a".to_string(),
+                    operator: given::RuleOperator::LessThan,
+                    rhs: "b".to_string(),
+                },
+            })),
         ];
         let expected = r#"---
 - ASIAN 101
+- course: ASIAN 101
+  term: 2014-1
+  section: ~
+  year: ~
+  semester: ~
+  lab: ~
+  international: ~
 - requirement: Name
-  optional: true"#;
+  optional: true
+- count: 1
+  of:
+    - ASIAN 101
+- both:
+    - ASIAN 101
+    - course: ASIAN 101
+      term: 2014-1
+      section: ~
+      year: ~
+      semester: ~
+      lab: ~
+      international: ~
+- either:
+    - ASIAN 101
+    - course: ASIAN 101
+      term: 2014-1
+      section: ~
+      year: ~
+      semester: ~
+      lab: ~
+      international: ~
+- given: courses
+  what: Courses
+  where: []
+  limit: []
+  do:
+    command: Count
+    lhs: a
+    operator: "<"
+    rhs: b"#;
 
         let actual = serde_yaml::to_string(&data).unwrap();
         assert_eq!(actual, expected);
     }
 
-    // #[test]
-    // fn deserialize() {
-    //     let data = "---\nname: Name\nmessage: ~\ndepartment_audited: false\nresult:\n  requirement: name\n  optional: false\ncontract: false\nsave: []\nrequirements: []";
-    //     let expected = Requirement {
-    //         name: String::from("Name"),
-    //         message: None,
-    //         department_audited: false,
-    //         result: Rule::Requirement(requirement::RequirementRule {
-    //             requirement: String::from("name"),
-    //             optional: false,
-    //         }),
-    //         contract: false,
-    //         save: vec![],
-    //         requirements: vec![],
-    //     };
+    #[test]
+    fn deserialize() {
+        let data = r#"---
+- ASIAN 101
+- course: ASIAN 101
+  term: 2014-1
+  section: ~
+  year: ~
+  semester: ~
+  lab: ~
+  international: ~
+- requirement: Name
+  optional: true
+- count: 1
+  of:
+    - ASIAN 101
+- both:
+    - ASIAN 101
+    - course: ASIAN 101
+      term: 2014-1
+      section: ~
+      year: ~
+      semester: ~
+      lab: ~
+      international: ~
+- either:
+    - ASIAN 101
+    - course: ASIAN 101
+      term: 2014-1
+      section: ~
+      year: ~
+      semester: ~
+      lab: ~
+      international: ~
+- given: courses
+  what: Courses
+  where: []
+  limit: []
+  do:
+    command: Count
+    lhs: a
+    operator: "<"
+    rhs: b"#;
+        let course_a = course::CourseRule {
+            course: "ASIAN 101".to_string(),
+            section: None,
+            term: None,
+            semester: None,
+            year: None,
+            international: None,
+            lab: None,
+        };
+        let course_b = course::CourseRule {
+            course: "ASIAN 101".to_string(),
+            section: None,
+            term: Some("2014-1".to_string()),
+            semester: None,
+            year: None,
+            international: None,
+            lab: None,
+        };
+        let expected = vec![
+            Rule::Course(course_a.clone()),
+            Rule::Course(course_b.clone()),
+            Rule::Requirement(requirement::RequirementRule {
+                requirement: "Name".to_string(),
+                optional: true,
+            }),
+            Rule::CountOf(count_of::CountOfRule {
+                count: count_of::CountOfEnum::Number(1),
+                of: vec![Rule::Course(course_a.clone())],
+            }),
+            Rule::Both(both::BothRule {
+                both: (
+                    Box::new(Rule::Course(course_a.clone())),
+                    Box::new(Rule::Course(course_b.clone())),
+                ),
+            }),
+            Rule::Either(either::EitherRule {
+                either: (
+                    Box::new(Rule::Course(course_a.clone())),
+                    Box::new(Rule::Course(course_b.clone())),
+                ),
+            }),
+            Rule::Given(given::GivenRule::AllCourses(given::GivenAllCoursesRule {
+                given: "courses".to_string(),
+                what: given::GivenWhatToGiveEnum::Courses,
+                filter: vec![],
+                limit: vec![],
+                action: given::DoAction {
+                    command: given::RuleAction::Count,
+                    lhs: "a".to_string(),
+                    operator: given::RuleOperator::LessThan,
+                    rhs: "b".to_string(),
+                },
+            })),
+        ];
 
-    //     let actual: Requirement = serde_yaml::from_str(&data).unwrap();
-    //     assert_eq!(actual, expected);
-    // }
-
-    // #[test]
-    // fn deserialize_with_defaults() {
-    //     let data = "---\nname: Name\nmessage: ~\nresult: {requirement: name, optional: false}\n";
-    //     let expected = Requirement {
-    //         name: String::from("Name"),
-    //         message: None,
-    //         department_audited: false,
-    //         result: Rule::Requirement(requirement::RequirementRule {
-    //             requirement: String::from("name"),
-    //             optional: false,
-    //         }),
-    //         contract: false,
-    //         save: vec![],
-    //         requirements: vec![],
-    //     };
-
-    //     let actual: Requirement = serde_yaml::from_str(&data).unwrap();
-    //     assert_eq!(actual, expected);
-    // }
+        let actual: Vec<Rule> = serde_yaml::from_str(&data).unwrap();
+        assert_eq!(actual, expected);
+    }
 }
