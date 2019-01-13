5a72c8066aaae4e9deccee79bbba4a269b91457f
Hawken Rives <hawkrives@fastmail.fm>
2019-01-13 12:40:58 -0600

    apply another clippy lint

--- printer-cli/src/main.rs
+++ printer-cli/src/main.rs
@@ -11,10 +11,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
 
-    let contents = fs::read_to_string(filename).expect(&format!(
-        "Something went wrong reading the file `{}`",
-        filename
-    ));
+    let contents = fs::read_to_string(filename)
+        .unwrap_or_else(|_| panic!("Something went wrong reading the file `{}`", filename));
 
     let area: AreaOfStudy = serde_yaml::from_str(&contents).unwrap();
 
