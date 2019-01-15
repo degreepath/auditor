e38c52d4e3f825340075a1f63f5b6af7ff0b5d2e
Hawken Rives <rives@stolaf.edu>
2019-01-15 14:00:30 -0600

    add more tests for Oxford

--- parser/src/util.rs
+++ parser/src/util.rs
@@ -177,3 +177,32 @@ where
 
     deserializer.deserialize_any(StringOrStruct(PhantomData))
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn oxford_len_eq0() {
+        use super::Oxford;
+        assert_eq!("", vec![].oxford("and"));
+    }
+
+    #[test]
+    fn oxford_len_eq1() {
+        use super::Oxford;
+        assert_eq!("A", vec!["A".to_string()].oxford("and"));
+    }
+
+    #[test]
+    fn oxford_len_eq2() {
+        use super::Oxford;
+        assert_eq!("A and B", vec!["A".to_string(), "B".to_string()].oxford("and"));
+        assert_eq!("A or B", vec!["A".to_string(), "B".to_string()].oxford("or"));
+    }
+
+    #[test]
+    fn oxford_len_gt3() {
+        use super::Oxford;
+        assert_eq!("A, B, and C", vec!["A".to_string(), "B".to_string(), "C".to_string()].oxford("and"));
+        assert_eq!("A, B, or C", vec!["A".to_string(), "B".to_string(), "C".to_string()].oxford("or"));
+    }
+}
