2713be4991651302ed7364deaf5e694151d1c0ac
Hawken Rives <hawkrives@fastmail.fm>
2019-01-20 15:40:25 -0600

    extract limit's tests

---
+++ parser/src/limit/tests.rs
@@ -0,0 +1,72 @@
+use super::*;
+
+#[test]
+fn serialize_level100() {
+	let clause: filter::Clause = indexmap! {
+		"level".into() => "100".parse::<filter::WrappedValue>().unwrap(),
+	};
+
+	let data = Limiter {
+		filter: clause,
+		at_most: 2,
+	};
+
+	let expected = r#"---
+where:
+  level:
+    Single:
+      op: EqualTo
+      value:
+        Integer: 100
+at_most: 2"#;
+
+	let actual = serde_yaml::to_string(&data).unwrap();
+
+	assert_eq!(actual, expected);
+}
+
+#[test]
+fn serialize_not_math() {
+	let clause: filter::Clause = indexmap! {
+		"department".into() => "! MATH".parse::<filter::WrappedValue>().unwrap(),
+	};
+
+	let data = Limiter {
+		filter: clause,
+		at_most: 2,
+	};
+
+	let expected = r#"---
+where:
+  department:
+    Single:
+      op: NotEqualTo
+      value:
+        String: MATH
+at_most: 2"#;
+
+	let actual = serde_yaml::to_string(&data).unwrap();
+
+	assert_eq!(actual, expected);
+}
+
+#[test]
+fn deserialize_level100() {
+	let clause: filter::Clause = indexmap! {
+		"level".into() => "100".parse::<filter::WrappedValue>().unwrap(),
+	};
+
+	let expected = Limiter {
+		filter: clause,
+		at_most: 2,
+	};
+
+	let data = r#"---
+where:
+  level: "100"
+at_most: 2"#;
+
+	let actual: Limiter = serde_yaml::from_str(&data).unwrap();
+
+	assert_eq!(actual, expected);
+}
