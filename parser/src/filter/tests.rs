6977328f3859e11e110e03fffb7fccaaf1fb550d
Hawken Rives <hawkrives@fastmail.fm>
2019-01-20 16:03:01 -0600

    reorganize some of the filter tests

--- parser/src/filter/tests.rs
+++ parser/src/filter/tests.rs
@@ -56,218 +56,253 @@ level:
 	assert_eq!(actual, expected);
 }
 
-#[test]
-fn deserialize_value_str() {
-	let data = "FYW";
-	let expected = Value::String("FYW".into());
-	let actual: Value = data.parse().unwrap();
-	assert_eq!(actual, expected);
+mod value {
+	use super::*;
+
+	#[test]
+	fn deserialize_value_str() {
+		let data = "FYW";
+		let expected = Value::String("FYW".into());
+		let actual: Value = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_value_int() {
+		let data = "1";
+		let expected = Value::Integer(1);
+		let actual: Value = data.parse().unwrap();
+		assert_eq!(actual, expected);
+
+		let data = "100";
+		let expected = Value::Integer(100);
+		let actual: Value = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_value_float() {
+		let data = "1.0";
+		let expected = Value::Float(1.0);
+		let actual: Value = data.parse().unwrap();
+		assert_eq!(actual, expected);
+
+		let data = "1.5";
+		let expected = Value::Float(1.5);
+		let actual: Value = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_value_bool() {
+		let data = "true";
+		let expected = Value::Bool(true);
+		let actual: Value = data.parse().unwrap();
+		assert_eq!(actual, expected);
+
+		let data = "false";
+		let expected = Value::Bool(false);
+		let actual: Value = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
 }
 
-#[test]
-fn deserialize_value_int() {
-	let data = "1";
-	let expected = Value::Integer(1);
-	let actual: Value = data.parse().unwrap();
-	assert_eq!(actual, expected);
+mod tagged_value {
+	use super::*;
 
-	let data = "100";
-	let expected = Value::Integer(100);
-	let actual: Value = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
+	#[test]
+	fn deserialize_untagged() {
+		let data = "FYW";
+		let expected = TaggedValue {
+			op: Operator::EqualTo,
+			value: Value::String("FYW".into()),
+		};
+		let actual: TaggedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
 
-#[test]
-fn deserialize_value_float() {
-	let data = "1.0";
-	let expected = Value::Float(1.0);
-	let actual: Value = data.parse().unwrap();
-	assert_eq!(actual, expected);
+	#[test]
+	fn deserialize_eq() {
+		let data = "= FYW";
+		let expected = TaggedValue {
+			op: Operator::EqualTo,
+			value: Value::String("FYW".into()),
+		};
+		let actual: TaggedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
 
-	let data = "1.5";
-	let expected = Value::Float(1.5);
-	let actual: Value = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_value_bool() {
-	let data = "true";
-	let expected = Value::Bool(true);
-	let actual: Value = data.parse().unwrap();
-	assert_eq!(actual, expected);
-
-	let data = "false";
-	let expected = Value::Bool(false);
-	let actual: Value = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_tagged_value_untagged() {
-	let data = "FYW";
-	let expected = TaggedValue {
-		op: Operator::EqualTo,
-		value: Value::String("FYW".into()),
-	};
-	let actual: TaggedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_tagged_value_eq() {
-	let data = "= FYW";
-	let expected = TaggedValue {
-		op: Operator::EqualTo,
-		value: Value::String("FYW".into()),
-	};
-	let actual: TaggedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_tagged_value_neq() {
-	let data = "! FYW";
-	let expected = TaggedValue {
-		op: Operator::NotEqualTo,
-		value: Value::String("FYW".into()),
-	};
-	let actual: TaggedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_tagged_value_gt() {
-	let data = "> FYW";
-	let expected = TaggedValue {
-		op: Operator::GreaterThan,
-		value: Value::String("FYW".into()),
-	};
-	let actual: TaggedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_tagged_value_gte() {
-	let data = ">= FYW";
-	let expected = TaggedValue {
-		op: Operator::GreaterThanEqualTo,
-		value: Value::String("FYW".into()),
-	};
-	let actual: TaggedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_tagged_value_lt() {
-	let data = "< FYW";
-	let expected = TaggedValue {
-		op: Operator::LessThan,
-		value: Value::String("FYW".into()),
-	};
-	let actual: TaggedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_tagged_value_lte() {
-	let data = "<= FYW";
-	let expected = TaggedValue {
-		op: Operator::LessThanEqualTo,
-		value: Value::String("FYW".into()),
-	};
-	let actual: TaggedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_wrapped_value() {
-	let data = "FYW";
-	let expected = WrappedValue::Single(TaggedValue {
-		op: Operator::EqualTo,
-		value: Value::String("FYW".into()),
-	});
-	let actual: WrappedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_wrapped_value_ne() {
-	let data = "! FYW";
-	let expected = WrappedValue::Single(TaggedValue {
-		op: Operator::NotEqualTo,
-		value: Value::String("FYW".into()),
-	});
-	let actual: WrappedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
-
-#[test]
-fn deserialize_wrapped_value_or_ne() {
-	let data = "! FYW | = FYW";
-	let expected = WrappedValue::Or(vec![
-		TaggedValue {
+	#[test]
+	fn deserialize_neq() {
+		let data = "! FYW";
+		let expected = TaggedValue {
 			op: Operator::NotEqualTo,
 			value: Value::String("FYW".into()),
-		},
-		TaggedValue {
-			op: Operator::EqualTo,
+		};
+		let actual: TaggedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_gt() {
+		let data = "> FYW";
+		let expected = TaggedValue {
+			op: Operator::GreaterThan,
 			value: Value::String("FYW".into()),
-		},
-	]);
-	let actual: WrappedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
+		};
+		let actual: TaggedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_gte() {
+		let data = ">= FYW";
+		let expected = TaggedValue {
+			op: Operator::GreaterThanEqualTo,
+			value: Value::String("FYW".into()),
+		};
+		let actual: TaggedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_lt() {
+		let data = "< FYW";
+		let expected = TaggedValue {
+			op: Operator::LessThan,
+			value: Value::String("FYW".into()),
+		};
+		let actual: TaggedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_lte() {
+		let data = "<= FYW";
+		let expected = TaggedValue {
+			op: Operator::LessThanEqualTo,
+			value: Value::String("FYW".into()),
+		};
+		let actual: TaggedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
 }
 
-#[test]
-fn deserialize_wrapped_value_or_untagged() {
-	let data = "FYW | FYW";
-	let expected = WrappedValue::Or(vec![
-		TaggedValue {
-			op: Operator::EqualTo,
-			value: Value::String("FYW".into()),
-		},
-		TaggedValue {
-			op: Operator::EqualTo,
-			value: Value::String("FYW".into()),
-		},
-	]);
-	let actual: WrappedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
+mod wrapped_value {
+	use super::*;
 
-#[test]
-fn deserialize_wrapped_value_and_untagged() {
-	let data = "FYW & FYW";
-	let expected = WrappedValue::And(vec![
-		TaggedValue {
+	#[test]
+	fn deserialize() {
+		let data = "FYW";
+		let expected = WrappedValue::Single(TaggedValue {
 			op: Operator::EqualTo,
 			value: Value::String("FYW".into()),
-		},
-		TaggedValue {
-			op: Operator::EqualTo,
-			value: Value::String("FYW".into()),
-		},
-	]);
-	let actual: WrappedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
-}
+		});
+		let actual: WrappedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
 
-#[test]
-fn deserialize_wrapped_value_and_ne() {
-	let data = "! FYW & = FYW";
-	let expected = WrappedValue::And(vec![
-		TaggedValue {
+	#[test]
+	fn deserialize_ne() {
+		let data = "! FYW";
+		let expected = WrappedValue::Single(TaggedValue {
 			op: Operator::NotEqualTo,
 			value: Value::String("FYW".into()),
-		},
-		TaggedValue {
+		});
+		let actual: WrappedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_or_ne() {
+		let data = "! FYW | = FYW";
+		let expected = WrappedValue::Or(vec![
+			TaggedValue {
+				op: Operator::NotEqualTo,
+				value: Value::String("FYW".into()),
+			},
+			TaggedValue {
+				op: Operator::EqualTo,
+				value: Value::String("FYW".into()),
+			},
+		]);
+		let actual: WrappedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_or_untagged() {
+		let data = "FYW | FYW";
+		let expected = WrappedValue::Or(vec![
+			TaggedValue {
+				op: Operator::EqualTo,
+				value: Value::String("FYW".into()),
+			},
+			TaggedValue {
+				op: Operator::EqualTo,
+				value: Value::String("FYW".into()),
+			},
+		]);
+		let actual: WrappedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_and_untagged() {
+		let data = "FYW & FYW";
+		let expected = WrappedValue::And(vec![
+			TaggedValue {
+				op: Operator::EqualTo,
+				value: Value::String("FYW".into()),
+			},
+			TaggedValue {
+				op: Operator::EqualTo,
+				value: Value::String("FYW".into()),
+			},
+		]);
+		let actual: WrappedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_and_ne() {
+		let data = "! FYW & = FYW";
+		let expected = WrappedValue::And(vec![
+			TaggedValue {
+				op: Operator::NotEqualTo,
+				value: Value::String("FYW".into()),
+			},
+			TaggedValue {
+				op: Operator::EqualTo,
+				value: Value::String("FYW".into()),
+			},
+		]);
+		let actual: WrappedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_multiword_single_value() {
+		let data = "St. Olaf College";
+		let expected = WrappedValue::Single(TaggedValue {
 			op: Operator::EqualTo,
-			value: Value::String("FYW".into()),
-		},
-	]);
-	let actual: WrappedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
+			value: Value::String("St. Olaf College".into()),
+		});
+		let actual: WrappedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
+
+	#[test]
+	fn deserialize_negated_multiword_single_value() {
+		let data = "! St. Olaf College";
+		let expected = WrappedValue::Single(TaggedValue {
+			op: Operator::NotEqualTo,
+			value: Value::String("St. Olaf College".into()),
+		});
+		let actual: WrappedValue = data.parse().unwrap();
+		assert_eq!(actual, expected);
+	}
 }
 
 fn deserialize_test(s: &str) -> Option<Clause> {
@@ -277,17 +312,6 @@ fn deserialize_test(s: &str) -> Option<Clause> {
 	let v: Wrapper = serde_yaml::from_str(s).unwrap();
 
 	v.0
-}
-
-#[test]
-fn deserialize_wrapped_value_multiword_single_value() {
-	let data = "St. Olaf College";
-	let expected = WrappedValue::Single(TaggedValue {
-		op: Operator::EqualTo,
-		value: Value::String("St. Olaf College".into()),
-	});
-	let actual: WrappedValue = data.parse().unwrap();
-	assert_eq!(actual, expected);
 }
 
 #[test]
