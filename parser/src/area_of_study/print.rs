3081ec8346ee36de1889a200e36189881180ab2a
Hawken Rives <hawkrives@fastmail.fm>
2019-01-27 14:10:37 -0600

    simplify printing the heading in an area of study

--- parser/src/area_of_study/print.rs
+++ parser/src/area_of_study/print.rs
@@ -8,60 +8,31 @@ impl AreaOfStudy {
 
 		use AreaType::{Concentration, Degree, Emphasis, Major, Minor};
 
-		let leader = match self.area_type.clone() {
-			Degree => {
-				let catalog = self.catalog.clone();
-				let area_name = self.area_name.clone();
-				let institution = "St. Olaf College";
-				let area_type = "degree".to_string();
-
-				format!("This is the set of requirements for the {catalog} “{area_name}” {area_type} from {institution}.", catalog = catalog, area_name = area_name, area_type = area_type, institution = institution)
-			}
-			Major { degree } => {
-				let catalog = self.catalog.clone();
-				let area_name = self.area_name.clone();
-				let institution = "St. Olaf College";
-				let area_type = "major".to_string();
-
-				format!("This is the set of requirements for the {catalog} {for_degree} “{area_name}” {area_type} from {institution}.", catalog = catalog, for_degree = degree, area_name = area_name, area_type = area_type, institution = institution)
-			}
-			Minor { degree } => {
-				let catalog = self.catalog.clone();
-				let area_name = self.area_name.clone();
-				let institution = "St. Olaf College";
-				let area_type = "minor".to_string();
-
-				format!("This is the set of requirements for the {catalog} {for_degree} “{area_name}” {area_type} from {institution}.", catalog = catalog, for_degree = degree, area_name = area_name, area_type = area_type, institution = institution)
-			}
-			Concentration { degree } => {
-				let catalog = self.catalog.clone();
-				let area_name = self.area_name.clone();
-				let institution = "St. Olaf College";
-				let area_type = "concentration".to_string();
-
-				format!("This is the set of requirements for the {catalog} {for_degree} “{area_name}” {area_type} from {institution}.", catalog = catalog, for_degree = degree, area_name = area_name, area_type = area_type, institution = institution)
-			}
-			Emphasis { degree, major } => {
-				let catalog = self.catalog.clone();
-				let area_name = self.area_name.clone();
-				let institution = "St. Olaf College";
-				let area_type = "area of emphasis".to_string();
-
-				format!("This is the set of requirements for the {catalog} {for_degree} {for_major} major's “{area_name}” {area_type} from {institution}.", catalog = catalog, for_degree = degree, for_major = major, area_name = area_name, area_type = area_type, institution = institution)
-			}
-		};
-
-		writeln!(&mut w, "> {}\n", leader)?;
-
-		writeln!(&mut w, "# {}", &self.area_name)?;
-
-		let area_type = match self.area_type.clone() {
+		let area_type = match &self.area_type {
 			Degree => "degree",
 			Major { .. } => "major",
 			Minor { .. } => "minor",
 			Concentration { .. } => "concentration",
 			Emphasis { .. } => "area of emphasis",
 		};
+
+		let leader = {
+			let catalog = &self.catalog;
+			let area_name = &self.area_name;
+			let institution = &self.institution.clone().unwrap_or("St. Olaf College".to_string());
+
+			match &self.area_type {
+                Degree => format!("This is the set of requirements for the {catalog} “{area_name}” {area_type} from {institution}.", catalog = catalog, area_name = area_name, area_type = area_type, institution = institution),
+                Major { degree } => format!("This is the set of requirements for the {catalog} {for_degree} “{area_name}” {area_type} from {institution}.", catalog = catalog, for_degree = degree, area_name = area_name, area_type = area_type, institution = institution),
+                Minor { degree } => format!("This is the set of requirements for the {catalog} {for_degree} “{area_name}” {area_type} from {institution}.", catalog = catalog, for_degree = degree, area_name = area_name, area_type = area_type, institution = institution),
+                Concentration { degree } => format!("This is the set of requirements for the {catalog} {for_degree} “{area_name}” {area_type} from {institution}.", catalog = catalog, for_degree = degree, area_name = area_name, area_type = area_type, institution = institution),
+                Emphasis { degree, major } => format!("This is the set of requirements for the {catalog} {for_degree} {for_major} major's “{area_name}” {area_type} from {institution}.", catalog = catalog, for_degree = degree, for_major = major, area_name = area_name, area_type = area_type, institution = institution),
+            }
+		};
+
+		writeln!(&mut w, "> {}\n", leader)?;
+
+		writeln!(&mut w, "# {}", &self.area_name)?;
 
 		if let Some(_attributes) = &self.attributes {
 			writeln!(&mut w, "> todo: this area has custom attributes defined.\n")?;
