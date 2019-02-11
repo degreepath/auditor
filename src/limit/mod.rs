d8e4f98c09b224f7a3d9009090abed5d27a4f558
Hawken Rives <hawkrives@fastmail.fm>
2019-02-10 23:18:37 -0600

    derive Hash,Eq,Ord,PartialOrd on Limiter

--- src/limit/mod.rs
+++ src/limit/mod.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 #[cfg(test)]
 mod tests;
 
-#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone, Hash, Eq, Ord, PartialOrd)]
 #[serde(deny_unknown_fields)]
 pub struct Limiter {
 	#[serde(rename = "where", deserialize_with = "filter::deserialize_with_no_option")]
