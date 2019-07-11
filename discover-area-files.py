2f9b1db2287aefbaf02f13df0a8a6c60dbf9ee8f
Hawken Rives <rives@stolaf.edu>
2019-07-11 15:22:05 -0500

    rework discover-area-files to cache and handle some errors

--- discover-area-files.py
+++ discover-area-files.py
@@ -1,4 +1,5 @@
 import argparse
+import functools
 import glob
 import json
 import os
@@ -29,29 +30,44 @@ def main():
             areas = data.get('areas', [])
             catalog = data['catalog']
 
+            if type(catalog) == str:
+                # TODO: better surface this error (or fix it)
+                continue
+
             degrees = set(a['degree'] for a in areas)
             for degree in degrees:
-                print(fname, "../areas/{}/{}/degree.yaml".format(str(catalog) + '-' + str(catalog + 1)[2:], degree))
+                area_path = "../areas/{}/{}/degree.yaml".format(
+                    str(catalog) + '-' + str(catalog + 1)[2:],
+                    degree,
+                )
+                print(fname, os.path.abspath(area_path))
 
             paths = (get_area_path(conn, a, catalog) for a in areas)
             for area_path in paths:
                 if not area_path:
                     continue
-                print(fname, "../areas/{}.yaml".format(area_path))
+
+                area_path = "../areas/{}.yaml".format(area_path)
+                print(fname, os.path.abspath(area_path))
 
 
+@functools.lru_cache(maxsize=None)
 def get_area_path(conn, area, catalog):
-    sql = """
-        SELECT concat_ws('/', catalog_year::text || '-' || substr((catalog_year + 1)::text, 3, 2), degree, type, filename) as path
-        FROM area
-        WHERE catalog_year = '2018' -- %(catalog)s
-          AND degree = %(degree)s
-          AND type = %(type)s
-          AND name = %(name)s
-    """
-
     with conn.cursor() as curs:
-        curs.execute(sql, {
+        curs.execute("""
+            SELECT concat_ws('/',
+                             catalog_year::text
+                                || '-'
+                                || substr((catalog_year + 1)::text, 3, 2),
+                             degree,
+                             type,
+                             filename) as path
+            FROM area
+            WHERE catalog_year = '2018' -- %(catalog)s
+              AND degree = %(degree)s
+              AND type = %(type)s
+              AND name = %(name)s
+        """, {
             'catalog': str(catalog),
             'degree': area['degree'],
             'type': area['kind'],
