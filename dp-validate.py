84278475b0611ca367652ba4d40b5be88a21d587
Hawken Rives <rives@stolaf.edu>
2019-07-27 19:51:20 -0500

    update dp-validate

--- dp-validate.py
+++ dp-validate.py
@@ -7,6 +7,7 @@ from degreepath import AreaOfStudy, Constants
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("areas", nargs="+")
+    parser.add_argument("--break", dest="break_on_err", action="store_true")
     args = parser.parse_args()
 
     for f in args.areas:
@@ -15,13 +16,16 @@ def main():
 
         try:
             c = Constants(matriculation_year=200)
-            area = AreaOfStudy.load(area_def, c)
+            area = AreaOfStudy.load(specification=area_def, c=c)
             area.validate()
         except Exception:
             print('!!\t{}'.format(f))
             traceback.print_exc()
             print()
             print()
+
+            if args.break_on_err:
+                break
 
 
 if __name__ == "__main__":
