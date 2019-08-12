6cc24bf7e768e5daf0e09e1a5da457169f5052d4
Hawken Rives <rives@stolaf.edu>
2019-08-12 09:25:51 -0500

    fix serialization of Requirements

--- degreepath/base/requirement.py
+++ degreepath/base/requirement.py
@@ -26,7 +26,7 @@ class BaseRequirementRule(Base):
             "name": self.name,
             "message": self.message,
             "result": self.result.to_dict() if self.result is not None else None,
-            "audited_by": self.audited_by,
+            "audited_by": self.audited_by.value if self.audited_by else None,
             "contract": self.is_contract,
         }
 
