f0fdec3a454dd472cfb20bb063b1a4bb1392db6a
Hawken Rives <rives@stolaf.edu>
2019-08-16 13:04:09 -0500

    add --quiet option

--- dp.py
+++ dp.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 logformat = "%(asctime)s %(name)s %(levelname)s %(message)s"
 
 
-def main() -> None:
+def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("--area", dest="area_files", nargs="+", required=True)
     parser.add_argument("--student", dest="student_files", nargs="+", required=True)
@@ -27,6 +27,7 @@ def main() -> None:
     parser.add_argument("--estimate", action='store_true')
     parser.add_argument("--transcript", action='store_true')
     parser.add_argument("--gpa", action='store_true')
+    parser.add_argument("-q", "--quiet", action='store_true')
     parser.add_argument("--tracemalloc-init", action='store_true')
     parser.add_argument("--tracemalloc-end", action='store_true')
     cli_args = parser.parse_args()
@@ -42,40 +43,55 @@ def main() -> None:
 
     for msg in dp['run'](args, transcript_only=cli_args.transcript):
         if isinstance(msg, NoStudentsMsg):
-            logger.critical('no student files provided')
+            if not cli_args.quiet:
+                logger.critical('no student files provided')
+            return 3
 
         elif isinstance(msg, NoAuditsCompletedMsg):
-            logger.critical('no audits completed')
+            if not cli_args.quiet:
+                logger.critical('no audits completed')
+            return 2
 
         elif isinstance(msg, AuditStartMsg):
-            print(f"auditing #{msg.stnum} against {msg.area_catalog} {msg.area_code}", file=sys.stderr)
+            if not cli_args.quiet:
+                print(f"auditing #{msg.stnum} against {msg.area_catalog} {msg.area_code}", file=sys.stderr)
 
         elif isinstance(msg, ExceptionMsg):
-            logger.critical("%s %s", msg.ex, msg.tb)
+            if not cli_args.quiet:
+                logger.critical("%s %s", msg.ex, msg.tb)
+            return 1
 
         elif isinstance(msg, ProgressMsg):
             if cli_args.tracemalloc_init:
                 snapshot = tracemalloc.take_snapshot()
                 display_top(snapshot)
-                return
+                return 0
 
-            avg_iter_s = sum(msg.recent_iters) / max(len(msg.recent_iters), 1)
-            avg_iter_time = pretty_ms(avg_iter_s * 1_000, format_sub_ms=True, unit_count=1)
-            print(f"{msg.count:,} at {avg_iter_time} per audit (best: {msg.best_rank})", file=sys.stderr)
+            if not cli_args.quiet:
+                avg_iter_s = sum(msg.recent_iters) / max(len(msg.recent_iters), 1)
+                avg_iter_time = pretty_ms(avg_iter_s * 1_000, format_sub_ms=True, unit_count=1)
+                print(f"{msg.count:,} at {avg_iter_time} per audit (best: {msg.best_rank})", file=sys.stderr)
 
         elif isinstance(msg, ResultMsg):
             if cli_args.tracemalloc_end:
                 snapshot = tracemalloc.take_snapshot()
                 display_top(snapshot)
-                return
+                return 0
 
-            print(result_str(msg, as_json=cli_args.json, as_raw=cli_args.raw, gpa_only=cli_args.gpa))
+            if not cli_args.quiet:
+                print(result_str(msg, as_json=cli_args.json, as_raw=cli_args.raw, gpa_only=cli_args.gpa))
+                return 0
 
         elif isinstance(msg, EstimateMsg):
-            print(f"estimated iterations: {msg.estimate:,}", file=sys.stderr)
+            if not cli_args.quiet:
+                print(f"estimated iterations: {msg.estimate:,}", file=sys.stderr)
 
         else:
-            logger.critical('unknown message %s', msg)
+            if not cli_args.quiet:
+                logger.critical('unknown message %s', msg)
+            return 1
+
+    return 0
 
 
 def result_str(msg: ResultMsg, *, as_json: bool = False, as_raw: bool = False, gpa_only: bool = False) -> str:
@@ -123,4 +139,4 @@ def display_top(snapshot, key_type='lineno', limit=10):
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
