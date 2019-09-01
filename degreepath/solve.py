0caf6dc3dc529744709c900207b70f77fe881663
Hawken Rives <hawkrives@fastmail.fm>
2019-08-31 22:00:02 -0500

    update CountRule to use the solve module

--- degreepath/solve.py
+++ degreepath/solve.py
@@ -6,8 +6,12 @@ if TYPE_CHECKING:
     from .context import RequirementContext
 
 
-def find_best_solution(*, rule: 'Rule', ctx: 'RequirementContext') -> Optional['Result']:
+def find_best_solution(*, rule: 'Rule', ctx: 'RequirementContext', reset_claims: bool = False) -> Optional['Result']:
     result = None
+
+    if reset_claims:
+        claims = ctx.claims
+        ctx.reset_claims()
 
     for s in rule.solutions(ctx=ctx):
         tmp_result = s.audit(ctx=ctx)
@@ -15,10 +19,17 @@ def find_best_solution(*, rule: 'Rule', ctx: 'RequirementContext') -> Optional['
         if result is None:
             result = tmp_result
 
+        if tmp_result.ok():
+            result = tmp_result
+            break
+
         if result.rank() < tmp_result.rank():
             result = tmp_result
 
-        if tmp_result.ok():
-            result = tmp_result
+        if reset_claims:
+            ctx.reset_claims()
+
+    if reset_claims:
+        ctx.set_claims(claims)
 
     return result
