1168887e294a1304563762b1629e6b9e2d94ace4
Hawken Rives <hawkrives@fastmail.fm>
2019-12-28 17:22:02 -0600

    fix loading of environment variables after dotenv is loaded

--- dp/server/__main__.py
+++ dp/server/__main__.py
@@ -12,6 +12,10 @@ import psycopg2  # type: ignore
 import psycopg2.extensions  # type: ignore
 import sentry_sdk
 
+# always resolve to the local .env file
+dotenv_path = Path(__file__).parent.parent.parent / '.env'
+dotenv.load_dotenv(verbose=True, dotenv_path=dotenv_path)
+
 from .audit import main as single
 
 logger = logging.getLogger(__name__)
@@ -20,10 +24,6 @@ if os.environ.get('SENTRY_DSN', None):
     sentry_sdk.init(dsn=os.environ.get('SENTRY_DSN'))
 else:
     logger.warning('SENTRY_DSN not set; skipping')
-
-# always resolve to the local .env file
-dotenv_path = Path(__file__).parent.parent.parent / '.env'
-dotenv.load_dotenv(verbose=True, dotenv_path=dotenv_path)
 
 AREA_ROOT = os.getenv('AREA_ROOT')
 
@@ -91,14 +91,14 @@ def process_queue(curs: psycopg2.extensions.cursor, pid: int) -> None:
 
         try:
             assert AREA_ROOT is not None, "The AREA_ROOT environment variable is required"
-            print(f'auditing #{queue_id}, stnum {student_id} against {area_catalog}/{area_code} with {pid}')
-
             area_path = os.path.join(AREA_ROOT, area_catalog, area_code + '.yaml')
+
+            print(f'auditing #{queue_id}, stnum {student_id} against {area_path} with {pid}')
             single(student_data=input_data, run_id=run_id, area_file=area_path)
             # language=PostgreSQL
             curs.execute('COMMIT;')
 
-            print(f'completed #{queue_id}, stnum {student_id} against {area_catalog}/{area_code} with {pid}')
+            print(f'completed #{queue_id}, stnum {student_id} against {area_path} with {pid}')
         except Exception as exc:
             # language=PostgreSQL
             curs.execute('ROLLBACK;')
