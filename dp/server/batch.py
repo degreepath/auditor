af7f8462dfd1862e87e0e4011317d6a201402ba1
Hawken Rives <hawkrives@fastmail.fm>
2019-12-28 17:41:59 -0600

    add some more logging to batch mode

--- dp/server/batch.py
+++ dp/server/batch.py
@@ -34,10 +34,13 @@ def fetch(stnum: str) -> Tuple[Dict, str]:
 
 
 def batch() -> Iterator[Tuple[Dict, str]]:
+    print('fetching stnums to audit')
     r = http.request('GET', BATCH_URL)
 
     student_ids = set(r.data.decode('utf-8').splitlines())
     student_ids.add('122932')
+
+    print(f'fetched {len(student_ids):,} stnums to audit')
 
     with ProcessPoolExecutor() as pool:
         future_to_stnum = {pool.submit(fetch, stnum): stnum for stnum in student_ids}
