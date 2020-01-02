4e967fd8e4a8f6dc998e6d67b47650754938502e
Hawken Rives <rives@stolaf.edu>
2020-01-02 08:45:37 -0600

    refactor the apply_operator function
    reduces the complexity and makes it much nicer to look at

--- dp/operator.py
+++ dp/operator.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Tuple, Union
 import enum
 import logging
 from functools import lru_cache
@@ -20,7 +20,7 @@ class Operator(enum.Enum):
         return str(self)
 
 
-@lru_cache(2048)  # noqa: C901
+@lru_cache(2048)
 def apply_operator(*, op: Operator, lhs: Any, rhs: Any) -> bool:
     """
     Applies two values (lhs and rhs) to an operator.
@@ -44,65 +44,17 @@ def apply_operator(*, op: Operator, lhs: Any, rhs: Any) -> bool:
     3. If LHS is a sequence, and OP is .EqualTo, OP is changed to .In
     4. If LHS is a sequence, and OP is .NotEqualTo, OP is changed to .NotIn
     """
-    debug = __debug__ and logger.isEnabledFor(logging.DEBUG)
-    if debug: logger.debug("lhs=`%s` op=%s rhs=`%s` (%s, %s)", lhs, op.name, rhs, type(lhs), type(rhs))
+
+    # logger.debug("lhs=`%s` op=%s rhs=`%s` (%s, %s)", lhs, op.name, rhs, type(lhs), type(rhs))
 
     if (lhs is None or rhs is None) and lhs != rhs:
         return False
 
     if isinstance(lhs, tuple) and isinstance(rhs, tuple):
-        if op is not Operator.In:
-            raise Exception('both rhs and lhs must not be sequences when using %s; lhs=%s, rhs=%s', op, lhs, rhs)
-
-        if len(lhs) == 0 or len(rhs) == 0:
-            if debug: logger.debug("either lhs=%s or rhs=%s was empty; returning false", len(lhs), len(rhs))
-            return False
-
-        if debug: logger.debug("converting both %s and %s to sets of strings, and running intersection", lhs, rhs)
-        lhs = set(str(s) for s in lhs)
-        rhs = set(str(s) for s in rhs)
-        intersection = lhs.intersection(rhs)
-        if debug: logger.debug("lhs=%s; rhs=%s; intersection=%s", lhs, rhs, intersection)
-        return len(intersection) > 0
+        return apply_operator__tuples(op=op, lhs=lhs, rhs=rhs)
 
     if isinstance(lhs, tuple) or isinstance(rhs, tuple):
-        if op is Operator.EqualTo:
-            if isinstance(lhs, tuple) and len(lhs) == 1:
-                if debug: logger.debug("got lhs=%s with one item, lifting out of the tuple", lhs)
-                lhs = lhs[0]
-            elif isinstance(lhs, tuple) and len(lhs) == 0:
-                if debug: logger.debug("got empty lhs, returning False")
-                return False
-            elif isinstance(rhs, tuple) and len(rhs) == 1:
-                if debug: logger.debug("got rhs=%s with one item, lifting out of the tuple", rhs)
-                rhs = rhs[0]
-            elif isinstance(rhs, tuple) and len(rhs) == 0:
-                if debug: logger.debug("got empty rhs, returning False")
-                return False
-            else:
-                if debug: logger.debug("got lhs=%s / rhs=%s; switching to %s", type(lhs), type(rhs), Operator.In)
-                return apply_operator(op=Operator.In, lhs=lhs, rhs=rhs)
-
-        elif op is Operator.NotEqualTo:
-            if debug: logger.debug("got lhs=%s / rhs=%s; switching to %s", type(lhs), type(rhs), Operator.NotIn)
-            return apply_operator(op=Operator.NotIn, lhs=lhs, rhs=rhs)
-
-        elif op is Operator.In:
-            if isinstance(lhs, tuple):
-                return any(apply_operator(op=Operator.EqualTo, lhs=v, rhs=rhs) for v in lhs)
-            if isinstance(rhs, tuple):
-                return any(apply_operator(op=Operator.EqualTo, lhs=lhs, rhs=v) for v in rhs)
-            raise TypeError(f"{op}: expected either {type(lhs)} or {type(rhs)} to be a tuple")
-
-        elif op is Operator.NotIn:
-            if isinstance(lhs, tuple):
-                return all(apply_operator(op=Operator.NotEqualTo, lhs=v, rhs=rhs) for v in lhs)
-            if isinstance(rhs, tuple):
-                return all(apply_operator(op=Operator.NotEqualTo, lhs=lhs, rhs=v) for v in rhs)
-            raise TypeError(f"{op}: expected either {type(lhs)} or {type(rhs)} to be a tuple")
-
-        else:
-            raise Exception(f'{op} does not accept a list; got {lhs} ({type(lhs)})')
+        return apply_operator__half_tuple(op=op, lhs=lhs, rhs=rhs)
 
     if isinstance(lhs, str) and not isinstance(rhs, str):
         rhs = str(rhs)
@@ -111,35 +63,102 @@ def apply_operator(*, op: Operator, lhs: Any, rhs: Any) -> bool:
 
     if op is Operator.EqualTo:
         result: bool = lhs == rhs
-        if debug: logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
+        # logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
         return result
 
-    if op is Operator.NotEqualTo:
+    elif op is Operator.NotEqualTo:
         result = lhs != rhs
-        if debug: logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
+        # logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
         return result
 
-    if op is Operator.LessThan:
+    elif op is Operator.LessThan:
         result = lhs < rhs
-        if debug: logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
+        # logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
         return result
 
-    if op is Operator.LessThanOrEqualTo:
+    elif op is Operator.LessThanOrEqualTo:
         result = lhs <= rhs
-        if debug: logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
+        # logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
         return result
 
-    if op is Operator.GreaterThan:
+    elif op is Operator.GreaterThan:
         result = lhs > rhs
-        if debug: logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
+        # logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
         return result
 
-    if op is Operator.GreaterThanOrEqualTo:
+    elif op is Operator.GreaterThanOrEqualTo:
         result = lhs >= rhs
-        if debug: logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
+        # logger.debug("`%s` %s `%s` == %s", lhs, op, rhs, result)
         return result
 
     raise TypeError(f"unknown comparison {op}")
+
+
+@lru_cache(2048)
+def apply_operator__tuples(*, op: Operator, lhs: Tuple, rhs: Tuple) -> bool:
+    if op is not Operator.In:
+        raise Exception('both rhs and lhs must not be sequences when using %s; lhs=%s, rhs=%s', op, lhs, rhs)
+
+    if (not lhs) or (not rhs):
+        # logger.debug("one of lhs=%s or rhs=%s was empty; returning false", len(lhs), len(rhs))
+        return False
+
+    # logger.debug("converting both %s and %s to sets of strings, and running intersection", lhs, rhs)
+    lhs_set = set(str(s) for s in lhs)
+    rhs_set = set(str(s) for s in rhs)
+    intersection = lhs_set.intersection(rhs_set)
+
+    # logger.debug("lhs=%s; rhs=%s; intersection=%s", lhs, rhs, intersection)
+    return bool(intersection)
+
+
+@lru_cache(2048)
+def apply_operator__half_tuple(*, op: Operator, lhs: Union[Tuple, Any], rhs: Union[Tuple, Any]) -> bool:
+    if op is Operator.EqualTo:
+        if isinstance(lhs, tuple):
+            lhs_len = len(lhs)
+            if lhs_len == 1:
+                # logger.debug("got lhs=%s with one item, lifting out of the tuple", lhs)
+                return apply_operator(op=Operator.EqualTo, lhs=lhs[0], rhs=rhs)
+            elif lhs_len == 0:
+                # logger.debug("got empty lhs, returning False")
+                return False
+
+        elif isinstance(rhs, tuple):
+            rhs_len = len(rhs)
+            if rhs_len == 1:
+                # logger.debug("got rhs=%s with one item, lifting out of the tuple", rhs)
+                return apply_operator(op=Operator.EqualTo, lhs=lhs, rhs=rhs[0])
+            elif rhs_len == 0:
+                # logger.debug("got empty rhs, returning False")
+                return False
+
+        # logger.debug("got lhs=%s / rhs=%s; switching to %s", type(lhs), type(rhs), Operator.In)
+        return apply_operator(op=Operator.In, lhs=lhs, rhs=rhs)
+
+    elif op is Operator.NotEqualTo:
+        # logger.debug("got lhs=%s / rhs=%s; switching to %s", type(lhs), type(rhs), Operator.NotIn)
+        return apply_operator(op=Operator.NotIn, lhs=lhs, rhs=rhs)
+
+    elif op is Operator.In:
+        if isinstance(lhs, tuple):
+            return any(apply_operator(op=Operator.EqualTo, lhs=v, rhs=rhs) for v in lhs)
+
+        elif isinstance(rhs, tuple):
+            return any(apply_operator(op=Operator.EqualTo, lhs=lhs, rhs=v) for v in rhs)
+
+        raise TypeError(f"{op}: expected either {type(lhs)} or {type(rhs)} to be a tuple")
+
+    elif op is Operator.NotIn:
+        if isinstance(lhs, tuple):
+            return all(apply_operator(op=Operator.NotEqualTo, lhs=v, rhs=rhs) for v in lhs)
+
+        elif isinstance(rhs, tuple):
+            return all(apply_operator(op=Operator.NotEqualTo, lhs=lhs, rhs=v) for v in rhs)
+
+        raise TypeError(f"{op}: expected either {type(lhs)} or {type(rhs)} to be a tuple")
+
+    raise Exception(f'{op} does not accept a list; got {lhs} ({type(lhs)})')
 
 
 def str_operator(op: str) -> str:
