c6407cb6c6d3158e71453ace547fd1d8a011445f
Hawken Rives <hawkrives@fastmail.fm>
2020-01-12 18:41:39 -0600

    optimize dp.solve
    now we only call .rank() on any given result object once, instead of
    calling it on the "best result" once per loop.

--- dp/solve.py
+++ dp/solve.py
@@ -1,4 +1,5 @@
-from typing import Optional, Dict, List, TYPE_CHECKING
+from typing import Optional, Dict, List, Union, TYPE_CHECKING
+from decimal import Decimal
 import logging
 
 if TYPE_CHECKING:  # pragma: no cover
@@ -13,6 +14,7 @@ def find_best_solution(*, rule: 'Rule', ctx: 'RequirementContext', merge_claims:
     logger.debug('solving rule at %s', rule.path)
 
     result: Optional['Result'] = None
+    rank: Union[int, Decimal] = 0
 
     claims: Dict[str, List['Claim']] = dict()
     if merge_claims:
@@ -21,16 +23,18 @@ def find_best_solution(*, rule: 'Rule', ctx: 'RequirementContext', merge_claims:
     with ctx.fresh_claims():
         for s in rule.solutions(ctx=ctx):
             inner_ctx = ctx.with_empty_claims()
+
             tmp_result = s.audit(ctx=inner_ctx)
+            tmp_rank = tmp_result.rank()
 
             if result is None:
-                result = tmp_result
+                result, rank = tmp_result, tmp_rank
 
-            if tmp_result.rank() > result.rank():
-                result = tmp_result
+            if tmp_rank > rank:
+                result, rank = tmp_result, tmp_rank
 
             if tmp_result.ok():
-                result = tmp_result
+                result, rank = tmp_result, tmp_rank
                 break
 
     if merge_claims and inner_ctx:
