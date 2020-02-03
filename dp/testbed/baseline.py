cb9996dd68b954f26350c80789af766b5ff9566d
Hawken Rives <rives@stolaf.edu>
2020-02-03 13:46:33 -0600

    multiply times by 2.5 when calculating timeout for baseline

--- dp/testbed/baseline.py
+++ dp/testbed/baseline.py
@@ -70,7 +70,7 @@ def baseline(args: argparse.Namespace) -> None:
                     (stnum, catalog, code),
                     db=args.db,
                     area_spec=area_specs[f"{catalog}/{code}"],
-                    timeout=float(minimum_duration.sec()),
+                    timeout=float(minimum_duration.sec()) * 2.5,
                 ): (stnum, catalog, code)
                 for (stnum, catalog, code) in records
             }
