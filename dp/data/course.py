2b20d27a756f36fa6654d7ea504c27863830ac5b
Hawken Rives <hawkrives@fastmail.fm>
2020-03-18 17:42:33 -0500

    avoid assuming clbids are integers

--- dp/data/course.py
+++ dp/data/course.py
@@ -105,7 +105,7 @@ class CourseInstance(Clausable):
         return self.identity_
 
     def verbose(self) -> str:
-        return f'{self.course_with_term()} "{self.name}" {self.credits} {self.grade_code.value} #{int(self.clbid)}'
+        return f'{self.course_with_term()} "{self.name}" {self.credits} {self.grade_code.value} #{self.clbid}'
 
     def course_with_term(self) -> str:
         if self.sub_type is SubType.Lab:
