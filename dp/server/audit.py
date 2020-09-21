a7ebc7bd0e1ca5cfda8fb64116f4206818291cf9
Hawken Rives <rives@stolaf.edu>
2020-09-21 15:25:54 -0500

    avoid creation of hundreds of dead postgres rows by only inserting the final version

--- dp/server/audit.py
+++ dp/server/audit.py
@@ -1,6 +1,6 @@
 # mypy: warn_unreachable = False
 
-from typing import Dict, Optional, cast
+from typing import Dict, Optional
 import json
 import logging
 
@@ -8,7 +8,6 @@ import psycopg2.extensions  # type: ignore
 from sentry_sdk import push_scope, capture_exception
 
 from dp.run import run
-from dp.ms import pretty_ms
 from dp.audit import ResultMsg, NoAuditsCompletedMsg, ProgressMsg, Arguments, EstimateMsg
 
 logger = logging.getLogger(__name__)
@@ -33,22 +32,10 @@ def audit(
     with push_scope() as outer_scope:
         outer_scope.user = {"id": stnum}
 
-        curs.execute("""
-            INSERT INTO result (  student_id,     area_code,     catalog,     run,     input_data,     expires_at,     link_only,   result_version)
-            VALUES             (%(student_id)s, %(area_code)s, %(catalog)s, %(run)s, %(input_data)s, %(expires_at)s, %(link_only)s, 2)
-            RETURNING id
-        """, {"student_id": stnum, "area_code": area_code, "catalog": area_catalog, "run": run_id, "input_data": json.dumps(student), "expires_at": expires_at, "link_only": link_only})
-
-        row = curs.fetchone()
-        result_id: int = cast(int, row[0])
-
-        logger.info(f"result id = {result_id}")
-
         with push_scope() as inner_scope:
             inner_scope.user = dict(id=stnum)
             inner_scope.set_tag("area_code", area_code)
             inner_scope.set_tag("catalog", area_catalog)
-            inner_scope.set_extra("result_id", result_id)
 
             try:
                 for msg in run(args, area_spec=area_spec, student=student):
@@ -59,15 +46,7 @@ def audit(
                         pass
 
                     elif isinstance(msg, ProgressMsg):
-                        avg_iter_time = pretty_ms(msg.avg_iter_ms, format_sub_ms=True)
-
-                        curs.execute("""
-                            UPDATE result
-                            SET iterations = %(count)s, duration = interval %(elapsed)s
-                            WHERE id = %(result_id)s
-                        """, {"result_id": result_id, "count": msg.iters, "elapsed": f"{msg.elapsed_ms}ms"})
-
-                        logger.info(f"{msg.iters:,} at {avg_iter_time} per audit")
+                        pass
 
                     elif isinstance(msg, ResultMsg):
                         result = msg.result.to_dict()
@@ -94,23 +73,60 @@ def audit(
                         # want the time when the computation was finished.
                         # see https://stackoverflow.com/a/24169018
                         curs.execute("""
-                            UPDATE result
-                            SET iterations = %(total_count)s
-                              , duration = interval %(elapsed)s
-                              , per_iteration = interval %(avg_iter_time)s
-                              , rank = %(rank)s
-                              , max_rank = %(max_rank)s
-                              , result = %(result)s::jsonb
-                              , ok = %(ok)s
-                              , ts = clock_timestamp()
-                              , gpa = %(gpa)s
-                              , claimed_courses = %(claimed_courses)s::jsonb
-                              , status = %(status)s
-                              , is_active = true
-                              , revision = coalesce((SELECT max(revision) FROM result WHERE student_id = %(student_id)s AND area_code = %(area_code)s), 0) + 1
-                            WHERE id = %(result_id)s
+                            INSERT INTO result (
+                                student_id,
+                                area_code,
+                                catalog,
+                                run,
+                                input_data,
+                                expires_at,
+                                link_only,
+                                result_version,
+                                iterations,
+                                duration,
+                                per_iteration,
+                                rank,
+                                max_rank,
+                                result,
+                                ok,
+                                ts,
+                                gpa,
+                                claimed_courses,
+                                status,
+                                is_active,
+                                revision
+                            )
+                            VALUES (
+                                %(student_id)s,
+                                %(area_code)s,
+                                %(catalog)s,
+                                %(run)s,
+                                %(input_data)s,
+                                %(expires_at)s,
+                                %(link_only)s,
+                                2,
+                                %(total_count)s,
+                                interval %(elapsed)s,
+                                interval %(avg_iter_time)s,
+                                %(rank)s,
+                                %(max_rank)s,
+                                %(result)s::jsonb,
+                                %(ok)s,
+                                clock_timestamp(),
+                                %(gpa)s,
+                                %(claimed_courses)s::jsonb,
+                                %(status)s,
+                                true,
+                                coalesce((SELECT max(revision) FROM result WHERE student_id = %(student_id)s AND area_code = %(area_code)s), 0) + 10
+                            )
                         """, {
-                            "result_id": result_id,
+                            "student_id": stnum,
+                            "area_code": area_code,
+                            "catalog": area_catalog,
+                            "run": run_id,
+                            "input_data": json.dumps(student),
+                            "expires_at": expires_at,
+                            "link_only": link_only,
                             "total_count": msg.iters,
                             "elapsed": f"{msg.elapsed_ms}ms",
                             "avg_iter_time": f"{msg.avg_iter_ms}ms",
@@ -121,8 +137,6 @@ def audit(
                             "gpa": result["gpa"],
                             "ok": result["ok"],
                             "status": result["status"],
-                            "student_id": stnum,
-                            "area_code": area_code,
                         })
 
                     else:
@@ -132,7 +146,35 @@ def audit(
                 capture_exception(ex)
 
                 curs.execute("""
-                    UPDATE result
-                    SET error = %(error)s
-                    WHERE id = %(result_id)s
-                """, {"result_id": result_id, "error": json.dumps({"error": str(ex)})})
+                    INSERT INTO result (
+                        student_id,
+                        area_code,
+                        catalog,
+                        run,
+                        input_data,
+                        expires_at,
+                        link_only,
+                        result_version,
+                        error
+                    )
+                    VALUES (
+                        %(student_id)s,
+                        %(area_code)s,
+                        %(catalog)s,
+                        %(run)s,
+                        %(input_data)s,
+                        %(expires_at)s,
+                        %(link_only)s,
+                        2,
+                        %(error)s
+                    )
+                """, {
+                    "student_id": stnum,
+                    "area_code": area_code,
+                    "catalog": area_catalog,
+                    "run": run_id,
+                    "input_data": json.dumps(student),
+                    "expires_at": expires_at,
+                    "link_only": link_only,
+                    "error": json.dumps({"error": str(ex)})
+                })
