36a76dedd387ce31b88c6a3a162895a1de5d9e0e
Hawken Rives <rives@stolaf.edu>
2020-10-14 21:12:54 -0500

    revise audit's insert-into-database logic to be kinder to the db

--- dp/server/audit.py
+++ dp/server/audit.py
@@ -30,122 +30,115 @@ def audit(
     stnum = student['stnum']
 
     logger.info("auditing #%s against %s %s", stnum, area_catalog, area_code)
-    with push_scope() as outer_scope, start_transaction(op="audit", name="audit") as sentry_transaction:
-        outer_scope.user = {"id": stnum}
+    with push_scope() as inner_scope:
+        inner_scope.user = dict(id=stnum)
+        inner_scope.set_tag("area_code", area_code)
+        inner_scope.set_tag("catalog", area_catalog)
 
-        with sentry_transaction.start_child(op="db", description="INSERT INTO result"):
-            curs.execute("""
-                INSERT INTO result (  student_id,     area_code,     catalog,     run,     input_data,     expires_at,     link_only,   result_version)
-                VALUES             (%(student_id)s, %(area_code)s, %(catalog)s, %(run)s, %(input_data)s, %(expires_at)s, %(link_only)s, 2)
-                RETURNING id
-            """, {"student_id": stnum, "area_code": area_code, "catalog": area_catalog, "run": run_id, "input_data": json.dumps(student), "expires_at": expires_at, "link_only": link_only})
+        try:
+            for msg in run(args, area_spec=area_spec, student=student):
+                if isinstance(msg, NoAuditsCompletedMsg):
+                    logger.critical('no audits completed')
 
-        row = curs.fetchone()
-        result_id: int = cast(int, row[0])
+                elif isinstance(msg, EstimateMsg):
+                    pass
 
-        logger.info(f"result id = {result_id}")
+                elif isinstance(msg, ProgressMsg):
+                    pass
 
-        with push_scope() as inner_scope:
-            inner_scope.user = dict(id=stnum)
-            inner_scope.set_tag("area_code", area_code)
-            inner_scope.set_tag("catalog", area_catalog)
-            inner_scope.set_extra("result_id", result_id)
+                elif isinstance(msg, ResultMsg):
+                    result = msg.result.to_dict()
+                    result_str = json.dumps(result)
 
-            try:
-                for msg in run(args, area_spec=area_spec, student=student):
-                    if isinstance(msg, NoAuditsCompletedMsg):
-                        logger.critical('no audits completed')
+                    # delete any old copies of this exact result
+                    curs.execute("""
+                        DELETE FROM result
+                        WHERE student_id = %(student_id)s AND area_code = %(area_code)s AND result = %(result)s::jsonb
+                    """, {"student_id": stnum, "area_code": area_code, "result": result_str})
 
-                    elif isinstance(msg, EstimateMsg):
-                        pass
+                    # deactivate all existing records
+                    curs.execute("""
+                        UPDATE result
+                        SET is_active = false
+                        WHERE
+                            student_id = %(student_id)s
+                            AND area_code = %(area_code)s
+                            AND is_active = true
+                    """, {"student_id": stnum, "area_code": area_code})
 
-                    elif isinstance(msg, ProgressMsg):
-                        with sentry_transaction.start_child(op="db", description="UPDATE iterations"):
-                            avg_iter_time = pretty_ms(msg.avg_iter_ms, format_sub_ms=True)
+                    # we use clock_timestamp() instead of now() here, because
+                    # now() is the start time of the transaction, and we instead
+                    # want the time when the computation was finished.
+                    # see https://stackoverflow.com/a/24169018
+                    curs.execute("""
+                        INSERT INTO result (
+                            student_id,
+                            area_code,
+                            catalog,
+                            run,
+                            input_data,
+                            expires_at,
+                            link_only,
+                            result_version,
+                            iterations,
+                            duration,
+                            per_iteration,
+                            rank,
+                            max_rank,
+                            result,
+                            ok,
+                            ts,
+                            gpa,
+                            claimed_courses,
+                            status,
+                            is_active,
+                            revision
+                        )
+                        VALUES (
+                            %(student_id)s,
+                            %(area_code)s,
+                            %(catalog)s,
+                            %(run)s,
+                            %(input_data)s,
+                            %(expires_at)s,
+                            %(link_only)s,
+                            2,
+                            %(total_count)s,
+                            interval %(elapsed)s,
+                            interval %(avg_iter_time)s,
+                            %(rank)s,
+                            %(max_rank)s,
+                            %(result)s::jsonb,
+                            %(ok)s,
+                            clock_timestamp(),
+                            %(gpa)s,
+                            %(claimed_courses)s::jsonb,
+                            %(status)s,
+                            true,
+                            coalesce((SELECT max(revision) FROM result WHERE student_id = %(student_id)s AND area_code = %(area_code)s), 0) + 1
+                        )
+                    """, {
+                        "student_id": stnum,
+                        "area_code": area_code,
+                        "catalog": area_catalog,
+                        "run": run_id,
+                        "input_data": json.dumps(student),
+                        "expires_at": expires_at,
+                        "link_only": link_only,
+                        "total_count": msg.iters,
+                        "elapsed": f"{msg.elapsed_ms}ms",
+                        "avg_iter_time": f"{msg.avg_iter_ms}ms",
+                        "result": result_str,
+                        "claimed_courses": json.dumps(msg.result.keyed_claims()),
+                        "rank": result["rank"],
+                        "max_rank": result["max_rank"],
+                        "gpa": result["gpa"],
+                        "ok": result["ok"],
+                        "status": result["status"],
+                    })
 
-                            curs.execute("""
-                                UPDATE result
-                                SET iterations = %(count)s, duration = interval %(elapsed)s
-                                WHERE id = %(result_id)s
-                            """, {"result_id": result_id, "count": msg.iters, "elapsed": f"{msg.elapsed_ms}ms"})
+                else:
+                    logger.critical('unknown message %s', msg)
 
-                            logger.info(f"{msg.iters:,} at {avg_iter_time} per audit")
-
-                    elif isinstance(msg, ResultMsg):
-                        result = msg.result.to_dict()
-                        result_str = json.dumps(result)
-
-                        with sentry_transaction.start_child(op="db", description="SELECT max(revision)"):
-                            # fetch the previous revision number
-                            curs.execute("""
-                                SELECT coalesce(max(revision), 0)
-                                FROM result
-                                WHERE student_id = %(student_id)s AND area_code = %(area_code)s
-                            """, {"student_id": stnum, "area_code": area_code})
-                            revision: int = curs.fetchone()
-
-                        with sentry_transaction.start_child(op="db", description="DELETE PRIOR COPIES"):
-                            # delete any old copies of this exact result
-                            curs.execute("""
-                                DELETE FROM result
-                                WHERE student_id = %(student_id)s AND area_code = %(area_code)s AND result = %(result)s::jsonb
-                            """, {"student_id": stnum, "area_code": area_code, "result": result_str})
-
-                        with sentry_transaction.start_child(op="db", description="DEACTIVATE"):
-                            # deactivate all existing records
-                            curs.execute("""
-                                UPDATE result
-                                SET is_active = false
-                                WHERE
-                                    student_id = %(student_id)s
-                                    AND area_code = %(area_code)s
-                                    AND is_active = true
-                            """, {"student_id": stnum, "area_code": area_code})
-
-                        with sentry_transaction.start_child(op="db", description="INSERT FINAL"):
-                            # we use clock_timestamp() instead of now() here, because
-                            # now() is the start time of the transaction, and we instead
-                            # want the time when the computation was finished.
-                            # see https://stackoverflow.com/a/24169018
-                            curs.execute("""
-                                UPDATE result
-                                SET iterations = %(total_count)s
-                                  , duration = interval %(elapsed)s
-                                  , per_iteration = interval %(avg_iter_time)s
-                                  , rank = %(rank)s
-                                  , max_rank = %(max_rank)s
-                                  , result = %(result)s::jsonb
-                                  , ok = %(ok)s
-                                  , ts = clock_timestamp()
-                                  , gpa = %(gpa)s
-                                  , claimed_courses = %(claimed_courses)s::jsonb
-                                  , status = %(status)s
-                                  , is_active = true
-                                  , revision = %(revision)s + 1
-                                WHERE id = %(result_id)s
-                            """, {
-                                "result_id": result_id,
-                                "total_count": msg.iters,
-                                "elapsed": f"{msg.elapsed_ms}ms",
-                                "avg_iter_time": f"{msg.avg_iter_ms}ms",
-                                "result": result_str,
-                                "claimed_courses": json.dumps(msg.result.keyed_claims()),
-                                "rank": result["rank"],
-                                "max_rank": result["max_rank"],
-                                "gpa": result["gpa"],
-                                "ok": result["ok"],
-                                "status": result["status"],
-                                "revision": revision,
-                            })
-
-                    else:
-                        logger.critical('unknown message %s', msg)
-
-            except Exception as ex:
-                capture_exception(ex)
-
-                curs.execute("""
-                    UPDATE result
-                    SET error = %(error)s
-                    WHERE id = %(result_id)s
-                """, {"result_id": result_id, "error": json.dumps({"error": str(ex)})})
+        except Exception as ex:
+            capture_exception(ex)
