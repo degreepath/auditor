f27f5b2910a119bc581e390b7f5106ef2b74d411
Hawken Rives <rives@stolaf.edu>
2020-11-18 15:37:41 -0600

    rewrite baseline on top of a generic batch function

--- dp/testbed/baseline.py
+++ dp/testbed/baseline.py
@@ -1,7 +1,9 @@
 from concurrent.futures import ProcessPoolExecutor, as_completed
+from typing import Optional
 import argparse
 import sqlite3
 
+import attr
 import tqdm  # type: ignore
 
 from .sqlite import sqlite_connect, sqlite_cursor
@@ -13,101 +15,124 @@ from dp.ms import pretty_ms, parse_ms_str
 
 
 def baseline(args: argparse.Namespace) -> None:
+    run_batch(args, baseline=True)
+
+
+@attr.s(frozen=True, auto_attribs=True)
+class Record:
+    stnum: str
+    catalog: str
+    code: str
+    duration: float
+    area_key: str
+
+
+def run_batch(args: argparse.Namespace, *, baseline: bool) -> None:
     fetch_if_needed(args)
 
     with sqlite_connect(args.db) as conn:
-        print('clearing baseline data... ', end='', flush=True)
-        conn.execute('DELETE FROM baseline')
+        if baseline:
+            print('clearing baseline data... ', end='', flush=True)
+            conn.execute('DELETE FROM baseline')
+        else:
+            print(f'clearing data for "{args.branch}"... ', end='', flush=True)
+            conn.execute('DELETE FROM branch WHERE branch = ?', [args.branch])
         conn.commit()
         print('cleared')
 
     minimum_duration = parse_ms_str(args.minimum_duration)
 
     with sqlite_connect(args.db) as conn:
-        results = conn.execute('''
-            SELECT
-                count(duration) as count,
-                coalesce(max(sum(duration) / :workers, max(duration)), 0) as duration_s
-            FROM server_data
-            WHERE duration < :min
-        ''', {'min': minimum_duration.sec(), 'workers': args.workers})
+        if baseline:
+            results = conn.execute('''
+                SELECT stnum, catalog, code, duration, catalog || '/' || code as area_key
+                FROM server_data
+                WHERE duration < :min
+                ORDER BY duration DESC, stnum, catalog, code
+            ''', {'min': minimum_duration.sec()})
+        else:
+            results = conn.execute('''
+                SELECT stnum, catalog, code, duration, catalog || '/' || code as area_key
+                FROM baseline
+                WHERE duration < :min
+                ORDER BY duration DESC, stnum, catalog, code
+            ''', {'min': minimum_duration.sec()})
 
-        count, estimated_duration_s = results.fetchone()
+        records = [Record(**r) for r in results]
 
-        pretty_min = pretty_ms(minimum_duration.ms())
-        pretty_dur = pretty_ms(estimated_duration_s * 1000)
-        print(f'{count:,} audits under {pretty_min} each: ~{pretty_dur} with {args.workers:,} workers')
+    if args.filter is not None:
+        records = [r for r in records if r.code == args.filter]
 
-        results = conn.execute('''
-            SELECT catalog, code
-            FROM server_data
-            WHERE duration < :min
-            GROUP BY catalog, code
-        ''', {'min': minimum_duration.sec()})
+    estimated_duration_s = sum(r.duration for r in records) / args.workers
+    pretty_dur = pretty_ms(estimated_duration_s * 1000)
+    pretty_min = pretty_ms(minimum_duration.ms())
+    print(f'{len(records):,} audits under {pretty_min} each: ~{pretty_dur} with {args.workers:,} workers')
 
-        area_specs = load_areas(args, list(results))
-
-        results = conn.execute('''
-            SELECT stnum, catalog, code
-            FROM server_data
-            WHERE duration < :min
-            ORDER BY duration DESC, stnum, catalog, code
-        ''', {'min': minimum_duration.sec()})
-
-        records = [(stnum, catalog, code) for stnum, catalog, code in results]
+    area_codes = set((r.catalog, r.code) for r in records)
+    area_specs = load_areas(args, [{"catalog": catalog, "code": code} for catalog, code in area_codes])
 
     remaining_records = list(records)
     print(f'running {len(records):,} audits...')
+
+    timeout: Optional[float] = None
+    if baseline:
+        timeout = float(minimum_duration.sec()) * 2.5
 
     with sqlite_connect(args.db) as conn, ProcessPoolExecutor(max_workers=args.workers) as executor:
         futures = {
             executor.submit(
                 audit,
-                (stnum, catalog, code),
+                (r.stnum, r.catalog, r.code),
                 db=args.db,
-                area_spec=area_specs[f"{catalog}/{code}"],
-                timeout=float(minimum_duration.sec()) * 2.5,
-            ): (stnum, catalog, code)
-            for (stnum, catalog, code) in records
-            if f"{catalog}/{code}" in area_specs
+                area_spec=area_specs[r.area_key],
+                timeout=timeout,
+            ): r
+            for r in records
+            if r.area_key in area_specs
         }
 
         pbar = tqdm.tqdm(total=len(futures), disable=None)
 
-        upcoming = [f"{stnum}:{code}" for stnum, _catalog, code in remaining_records[:args.workers]]
+        upcoming = [f"{r.stnum}:{r.code}" for r in remaining_records[:args.workers]]
         pbar.set_description(', '.join(upcoming))
 
         for future in as_completed(futures):
-            stnum, catalog, code = futures[future]
+            record = futures[future]
 
             try:
-                remaining_records.remove((stnum, catalog, code))
-                upcoming = [f"{stnum}:{code}" for stnum, _catalog, code in remaining_records[:args.workers]]
+                remaining_records.remove(record)
+                upcoming = [f"{r.stnum}:{r.code}" for r in remaining_records[:args.workers]]
             except ValueError:
                 pass
 
             pbar.update(n=1)
-            pbar.write(f"completed ({stnum}, {code})")
+            # pbar.write(f"completed ({record.stnum}, {record.code})")
             pbar.set_description(', '.join(upcoming))
 
             try:
                 db_args = future.result()
-            except TimeoutError as timeout:
-                print(timeout.args[0])
+            except TimeoutError as err:
+                print(err.args[0])
                 conn.commit()
                 continue
             except Exception as exc:
-                print(f'{stnum} {catalog} {code} generated an exception: {exc}')
+                print(f'{record.stnum} {record.catalog} {record.code} generated an exception: {exc}')
                 continue
 
-            assert db_args is not None
+            assert db_args is not None, f"{record.stnum}, {record.catalog}, {record.code} returned None"
 
             with sqlite_cursor(conn) as curs:
                 try:
-                    curs.execute('''
-                        INSERT INTO baseline (stnum, catalog, code, iterations, duration, gpa, ok, rank, max_rank, status, result)
-                        VALUES (:stnum, :catalog, :code, :iterations, :duration, :gpa, :ok, :rank, :max_rank, :status, json(:result))
-                    ''', db_args)
+                    if baseline:
+                        curs.execute('''
+                            INSERT INTO baseline (stnum, catalog, code, iterations, duration, gpa, ok, rank, max_rank, status, result)
+                            VALUES (:stnum, :catalog, :code, :iterations, :duration, :gpa, :ok, :rank, :max_rank, :status, json(:result))
+                        ''', db_args)
+                    else:
+                        curs.execute('''
+                            INSERT INTO branch (branch, stnum, catalog, code, iterations, duration, gpa, ok, rank, max_rank, status, result)
+                            VALUES (:run, :stnum, :catalog, :code, :iterations, :duration, :gpa, :ok, :rank, :max_rank, :status, json(:result))
+                        ''', db_args)
                 except sqlite3.Error as ex:
                     print(db_args)
                     print(db_args['stnum'], db_args['catalog'], db_args['code'], 'generated an exception', ex)
